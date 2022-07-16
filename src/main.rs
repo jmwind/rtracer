bce091b0579f5106ee3b44f5ea82e96e4bae2d71
Jean-Michel Lemieux <jmwind@gmail.com>
2022-07-16 13:00:13 -0400

    more

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use std::fmt::Display;
+use std::fmt;
 
 enum TuppleType {
-    Point,
+    Point = 1,
     Vector,
 }
 
@@ -12,21 +14,38 @@ struct Tupple {
 }
 
 impl Tupple {
-    pub fn Add(&mut self, other: &Tupple) {
+    pub fn vector(x:u64, y:u64, z:u64) -> Tupple {
+        Tupple {x: x, y: y, z: z, t: TuppleType::Vector}
+    }
+
+    pub fn point(x:u64, y:u64, z:u64) -> Tupple {
+        Tupple {x: x, y: y, z: z, t: TuppleType::Point}
+    }
+
+    pub fn add(&mut self, other: &Tupple) {
+        self.x = self.x + other.x;
+        self.y = self.y + other.y;
+        self.z = self.z + other.z;
 
     }
 
-    pub fn Sub(&mut self, other: &Tupple) {
+    pub fn sub(&mut self, other: &Tupple) {
 
     }
 }
 
-fn main() {
-    println!("Hello, world!");
-    let mut guess = String::from("This is a string");
-    let apples = 5;
-    let t1 = (1,2,3);
-    let t2 = (4,5,6);
-    
-    print!("this is {} apples of {} and {}", apples, guess, t3);
+impl Display for Tupple {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let type_string = match self.t { TuppleType::Point => "Point", TuppleType::Vector => "Vector" };
+        write!(f, "({}, {}, {}, {})", self.x, self.y, self.z, type_string)
+    } 
+}
+
+fn main() {    
+    let p1 = Tupple::point(0, 1, 1);
+    let p2: Tupple = Tupple::point(0, 1, 1);
+    let v1 = Tupple::vector(4, 2, 1);
+    let v2 = Tupple::vector(8, 12, 11);
+
+    println!("p1 {} p2 {} p3 {} p4 {}", p1,p2,v1,v2);
 }
