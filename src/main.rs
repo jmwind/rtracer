b3a32bed0048479e144b757a15ddf7925e42d1a8
Jean-Michel Lemieux <jmwind@gmail.com>
2022-07-26 14:37:04 -0400

    minifb test and vectors complete

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::fmt;
 use std::fmt::Display;
+use minifb::{Key, Window, WindowOptions};
 
 const EPSILON: f64 = 0.00001;
 const POINT: f64 = 1.0;
@@ -64,10 +65,43 @@ impl Tupple {
         }
     }
 
+    pub fn mul(&mut self, factor: f64) {
+        self.x = factor * self.x;
+        self.y = factor * self.y;
+        self.z = factor * self.z;
+        self.w = factor * self.w;
+    }
+
     pub fn negate(&mut self) {
         self.x = -1.0 * self.x;
         self.y = -1.0 * self.y;
         self.z = -1.0 * self.z;
+        self.w = -1.0 * self.w;
+    }
+
+    pub fn magnitude(&self) -> f64 {
+        let m = self.x.powi(2) + self.y.powi(2) + self.z.powi(2) + self.w.powi(2);
+        return m.abs().sqrt();
+    }
+
+    pub fn normalize(&self) -> Tupple {
+        let m = self.magnitude();
+        return Tupple::tupple(self.x / m, self.y / m, self.z / m, self.w / m);
+    }
+
+    pub fn dot(&self, _other: &Tupple) -> f64 {
+        return 
+            self.x * _other.x +
+            self.y * _other.y +
+            self.z * _other.z +
+            self.w * _other.w;
+    }
+
+    pub fn cross(&self, _other: &Tupple) -> Tupple {
+        return Tupple::vector(
+            self.y * _other.z - self.z * _other.y,            
+            self.z * _other.x - self.x * _other.z,
+            self.x * _other.y - self.y * _other.x);
     }
 }
 
@@ -86,13 +120,34 @@ impl Display for Tupple {
     }
 }
 
+const WIDTH: usize = 900;
+const HEIGHT: usize = 550;
 fn main() {
-    let p1 = Tupple::point(0.0, 1.0, 1.0);
-    let p2: Tupple = Tupple::point(0.0, 1.0, 1.0);
-    let v1 = Tupple::vector(4.0, 2.0, 1.0);
-    let v2 = Tupple::vector(8.0, 12.0, 11.0);
+    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
 
-    println!("p1 {} p2 {} p3 {} p4 {}", p1, p2, v1, v2);
+    let mut window = Window::new(
+        "Test - ESC to exit",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default(),
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
+
+    // Limit to max ~60 fps update rate
+    window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        for i in buffer.iter_mut() {
+            *i = 122; // write something more funny here!
+        }
+
+        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
+        window
+            .update_with_buffer(&buffer, WIDTH, HEIGHT)
+            .unwrap();
+    }
 }
 
 // ## TESTS ##############################
@@ -193,5 +248,81 @@ mod tests {
         let p1 = Tupple::point(3.0, -2.0, 5.0);
         let mut v1 = Tupple::vector(-2.0, 3.0, 1.0);
         v1.sub(&p1);
+    }
+
+    #[test]
+    fn test_negate() {        
+        let mut v1 = Tupple::tupple(1.0, -2.0, 3.0, -4.0);
+        v1.negate();
+        assert_eq!(v1.x, -1.0);
+        assert_eq!(v1.y, 2.0);
+        assert_eq!(v1.z, -3.0);
+        assert_eq!(v1.w, 4.0);
+    }
+
+    #[test]
+    fn test_multiply() {
+        let mut v1 = Tupple::tupple(1.0, -2.0, 3.0, -4.0);
+        v1.mul(3.5);
+        assert_eq!(v1.x, 3.5);
+        assert_eq!(v1.y, -7.0);
+        assert_eq!(v1.z, 10.5);
+        assert_eq!(v1.w, -14.0);
+
+        let mut v2 = Tupple::tupple(1.0, -2.0, 3.0, -4.0);
+        v2.mul(0.5);
+        assert_eq!(v2.x, 0.5);
+        assert_eq!(v2.y, -1.0);
+        assert_eq!(v2.z, 1.5);
+        assert_eq!(v2.w, -2.0);
+    }
+
+    #[test]
+    fn test_magnitude() {
+        let v1 = Tupple::vector(0.0, 1.0, 0.0);
+        let v2 = Tupple::vector(1.0, 0.0, 0.0);
+        let v3 = Tupple::vector(0.0, 0.0, 1.0);
+        let v4 = Tupple::vector(1.0, 2.0, 3.0);
+        let v5 = Tupple::vector(-1.0, -2.0, -3.0);
+        assert_eq!(v1.magnitude(), 1.0);
+        assert_eq!(v2.magnitude(), 1.0);
+        assert_eq!(v3.magnitude(), 1.0);
+        assert_eq!(v4.magnitude(), 14.0_f64.sqrt());
+        assert_eq!(v5.magnitude(), 14.0_f64.sqrt());
+
+    }
+
+    #[test]
+    fn test_normalize() {
+        let v1 = Tupple::vector(4.0, 0.0, 0.0).normalize();
+        let v2 = Tupple::vector(1.0, 2.0, 3.0).normalize();
+        assert_eq!(v1.x, 1.0);
+        assert_eq!(v1.y, 0.0);
+        assert_eq!(v1.z, 0.0);
+        assert_eq!(v2.x, 1.0 / 14.0_f64.sqrt());
+        assert_eq!(v2.y, 2.0 / 14.0_f64.sqrt());
+        assert_eq!(v2.z, 3.0 / 14.0_f64.sqrt());
+    }
+
+    #[test]
+    fn test_dot() {
+        let v1 = Tupple::vector(1.0, 2.0, 3.0);
+        let v2 = Tupple::vector(2.0, 3.0, 4.0);
+        let dot = v1.dot(&v2);
+        assert_eq!(dot, 20.0);        
+    }
+
+    #[test]
+    fn test_cross() {
+        let v1 = Tupple::vector(1.0, 2.0, 3.0);
+        let v2 = Tupple::vector(2.0, 3.0, 4.0);
+        let c1 = v1.cross(&v2);
+        let c2 = v2.cross(&v1);
+        assert_eq!(c1.x, -1.0);
+        assert_eq!(c1.y, 2.0);
+        assert_eq!(c1.z, -1.0);
+        assert_eq!(c2.x, 1.0);
+        assert_eq!(c2.y, -2.0);
+        assert_eq!(c2.z, 1.0);
     }
 }
